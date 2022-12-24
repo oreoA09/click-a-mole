592a375961f6b8c16d511bffbe1d535943916a62
oreoA09 <oreo.akanni@gmail.com>
2022-12-24 22:06:50 +0000

    paly audio onClick

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import { React, useState } from 'react';
-import MoleContainer from './MoleContainer';
-import './App.css'
-
+import React, { useState } from "react";
+import MoleContainer from "./MoleContainer";
+import "./App.css";
 
 function App() {
-
-  let [score, setScore] = useState(0)
+  let [score, setScore] = useState(0);
 
   const createMoleHill = () => {
     let hills = [];
-    
-    for(let i = 0; i < 9; i++){
-      hills.push(
-      <MoleContainer
-      key={i}
-      setScore={setScore}
-      score={score}
-      />
-      )
+
+    for (let i = 0; i < 8; i++) {
+      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />);
     }
 
-    return (
-      <div>
-        {hills}
-      </div>
-    )
-  }
+    return <div>{hills}</div>;
+  };
+
+  // console.log(score)
 
   return (
     <div className="App">
       <h1>Click-a-Mole!</h1>
-      <b>   {score}</b>
+      <b> {score}</b>
       {createMoleHill()}
     </div>
   );
--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -1,21 +1,27 @@
-import { React, useState } from "react";
-import Mole from './Mole';
-import EmptySlot from './EmptySlot'
+import React, { useState } from "react";
+import Mole from "./Mole";
+import EmptySlot from "./EmptySlot";
+import soundFile from "./bash.mp3";
 
-function MoleContainer(props){
+function MoleContainer(props) {
+  let [displayMole, setDisplayMole] = useState(false);
 
-    let [displayMole,setDisplayMole] = useState(false);
+  const bashAudio = new Audio(soundFile);
 
-    const moleClicked = () => {
-        props.setScore(props.score +1)
-        setDisplayMole(false)
-    }
-    return (
-        <div style={{"display": "inline-block"}}>
-            {displayMole ? <Mole setDisplayMole={setDisplayMole} 
-            moleClicked={moleClicked}/> : <EmptySlot setDisplayMole={setDisplayMole}/>}
-        </div>
-    )
+  const moleClicked = () => {
+    props.setScore(props.score + 1);
+    setDisplayMole(false);
+    bashAudio.play();
+  };
+  return (
+    <div style={{ display: "inline-block" }}>
+      {displayMole ? (
+        <Mole setDisplayMole={setDisplayMole} moleClicked={moleClicked} />
+      ) : (
+        <EmptySlot setDisplayMole={setDisplayMole} />
+      )}
+    </div>
+  );
 }
 
-export default MoleContainer
+export default MoleContainer;
